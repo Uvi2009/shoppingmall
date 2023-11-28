3ace7b42dfa183f3af570c522f98142c8769c388
yuvar <yuvar@DESKTOP-270VS22>
2023-11-28 07:46:26 +0530

    hlo

--- OrderModule/src/main/java/com/Shopping/Repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Shopping.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Shopping.Entity.OrderEnity;
-
-public interface OrderRepository extends JpaRepository<OrderEnity, Integer> {
-
-}
