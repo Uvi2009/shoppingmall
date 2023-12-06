24c1a405e0f0737dd9cd5aab2a54907293141dbe
Uvi2009 <128220846+Uvi2009@users.noreply.github.com>
2023-12-06 08:56:26 +0530

    Update ShoppingController.java

--- ShoppingMall/src/main/java/com/shopping/Controller/ShoppingController.java
+++ ShoppingMall/src/main/java/com/shopping/Controller/ShoppingController.java
@@ -69,14 +69,6 @@ public class ShoppingController {
 		}
 	}
 
-	@PatchMapping("/customer-detail/{id}")
-	public void updateData1(@PathVariable Integer id, @RequestBody Mall mall) {
-		try {
-		 service.save(mall); 
-		}
-		catch(Exception e) {
-			
-		}
-	}
+	
 
 }
